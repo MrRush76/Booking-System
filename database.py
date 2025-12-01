e91fb9d402589a8421233df882e106d92830ec15
MrRush <98553195+MrRush76@users.noreply.github.com>
2025-12-01 15:58:52 +0000

    Refactor database table creation methods

--- database.py
+++ database.py
@@ -13,26 +13,25 @@ class Database():
         db.close()
         return result
 
-def create_tables(db):
+def create_table_user(db):
     db.execute(
-        "CREATE TABLE IF NOT EXISTS Users ( User_ID INTEGER PRIMARY KEY, Email TEXT NOT NULL, Username TEXT , Password TEXT NOT NULL, Admin BOOLEAN NOT NULL DEFAULT false )")
+        "CREATE TABLE IF NOT EXISTS Users ( User_ID INTEGER PRIMARY KEY, Email TEXT NOT NULL, Username TEXT , Password TEXT NOT NULL, Admin BOOLEAN NOT NULL DEFAULT false )"
+    )
+def create_table_bookings(db):
+    db.execute(
+        "CREATE TABLE IF NOT EXISTS Bookings ( Booking_ID INTEGER PRIMARY KEY, User_ID INTEGER REFERENCES Users(User_ID), DATE TEXT NOT NULL, TIME TEXT NOT NULL )"
+    )
 
-class user_database(Database):
+class user_table(Database):
     def __init__(self, db_path):
         super().__init__(db_path)
         self.connection = sqlite3.connect(db_path)
         self.cursor = self.connection.cursor()
-        self.create_tables()
+        self.create_table()
 
     def execute(self, query, params=()):
         self.cursor.execute(query, params)
         return self.cursor
-
-    def create_tables(self):
-        db = sqlite3.connect(self.databaseRef)
-        create_tables(db)
-        db.close()
-
 
     def check_account(self,email, username, password):
         db = sqlite3.connect(self.databaseRef)
@@ -44,6 +43,13 @@ class user_database(Database):
             return result[0][0]
         return False
 
+    def create_table(self):
+        create_table_user(self)
+
+    def check_admin(self, email, username, password):
+        db = sqlite3.connect(self.databaseRef)
+        data = db.execute("SELECT Admin FROM Users WHERE Email = ? AND Username = ? AND Password = ?", (email, username, hash(password)))
+        return data
     def add_user(self,email, username, password, admin=False):
         if not self.check_account(email, username, password):
             db = sqlite3.connect(self.databaseRef)
@@ -58,3 +64,21 @@ class user_database(Database):
                 (user_id,email, username, hash(password),admin))
             db.commit()
             db.close()
+
+class booking_database:
+    def __init__(self, db_path):
+        super().__init__(db_path)
+        self.connection = sqlite3.connect(db_path)
+        self.cursor = self.connection.cursor()
+        self.create_tables()
+
+    def execute(self, query, params=()):
+        self.cursor.execute(query, params)
+        return self.cursor
+
+    def create_tables(self):
+        create_table_bookings(self)
+
+    def get_available_bookings(self, date, opening_times):
+        bookings = self.execute("SELECT Date AND Time FROM Bookings WHERE Date = ?", (date,)).fetchall()
+        available_bookings = 
